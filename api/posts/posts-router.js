969912cb4c9394b7fd180ef4eee6c8415e119e68
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-10-31 23:26:10 -0400

    added second get request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,16 +2,21 @@
 const express = require('express');
 
 const router = express.Router();
+const posts = require('../posts/posts-model');
 
 router.get('/api/posts', (req, res) => {
     if(!req.body){
         res.status(500).json({ message: "The posts information could not be retrieved" });
     }
     else{
-        res.status(200).send("Success");
+        res.status(200).send(posts);
     }
 });
 
 router.get('/api/posts/:id', (req, res) => {
     //
+    const id = req.params.id;
+    /*if(){
+        //
+    }*/
 });
