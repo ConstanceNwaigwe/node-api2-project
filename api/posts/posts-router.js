57cb3e9013980197618a8b34bf913af4c2388185
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-10-31 23:07:04 -0400

    added get request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/api/posts', (req, res) => {
+    if(!req.body){
+        res.status(500).json({ message: "The posts information could not be retrieved" });
+    }
+    else{
+        res.status(200).send("Success");
+    }
+});
+
+router.get('/api/posts/:id', (req, res) => {
+    //
+});
--- api/server.js
+++ api/server.js
@@ -1,2 +1,6 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const router = express.Router();
+
+module.exports = router
