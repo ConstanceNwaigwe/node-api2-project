6d86f9c40dcfe8ba9884be3b662cf70e689cbbdd
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-05 21:47:14 -0400

    updated server file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express');
-const router = express.Router();
+//const router = express.Router();
+const server = express();
+server.use(express.json());
+server.use('*', (req,res) => {
+    res.status(404).json({message: 'not found'})
+})
 
-module.exports = router
+module.exports = server
